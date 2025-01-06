f5e46d71723013ea0f19cf0b03b067a7442e9d40
ycovich <ycovich22@gmail.com>
2025-01-07 01:51:41 +0300

    fix: typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import {TonConnectUIProvider} from '@tonconnect/ui-react';
 
-// TODO Replace with your manifest URL after deploying to GitHub Pages
-const manifestUrl = 'https://https://ycovich.github.io/dice-bot-tma/manifest.json';
+const manifestUrl = 'https://ycovich.github.io/dice-city-stories-tma/manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <TonConnectUIProvider manifestUrl={manifestUrl}>
-            <App />
+            <App/>
         </TonConnectUIProvider>
     </React.StrictMode>,
 );
