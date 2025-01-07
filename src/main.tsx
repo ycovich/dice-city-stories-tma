6fe5864a4a631d7f93baf5d3f602aed3998b4b3c
ycovich <ycovich22@gmail.com>
2025-01-07 20:40:59 +0300

    fix: css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {TonConnectUIProvider} from '@tonconnect/ui-react';
 import './index.css';  // Importing global styles
-import './app.css';    // Importing app-specific styles
 
 const manifestUrl = 'https://ycovich.github.io/dice-city-stories-tma/manifest.json';
 
